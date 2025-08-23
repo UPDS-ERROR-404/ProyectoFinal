064ac81e11d56064f6b6336f722a55b98046f30e
SCPC 1020 <SCPC1020@labs.upds>
2025-08-22 20:59:08 -0400

    creacion de las clases tickets y parking

--- Error404-Parkiando/Error404-Parkiando/Domain/Ticket.cs
+++ Error404-Parkiando/Error404-Parkiando/Domain/Ticket.cs
@@ -1,6 +1,17 @@
-﻿namespace Error404_Parkiando.Domain
+﻿public class Ticket
 {
-    public class Ticket
+    public int Id { get; set; }
+    public string Plate { get; set; } = null!;
+    public int ParkingSpotId { get; set; }
+    public DateTime CheckIn { get; set; } = DateTime.Now;
+    public DateTime? CheckOut { get; set; }
+    public decimal? Amount { get; set; }
+}
+
+public class ParkingSpot
 {
+    public int Id { get; set; }
+    public string Code { get; set; } = null!;
+    public SpotStatus Status { get; set; } = SpotStatus.Free;
 }
-}
+public enum SpotStatus { Free, Occupied, Reserved }
