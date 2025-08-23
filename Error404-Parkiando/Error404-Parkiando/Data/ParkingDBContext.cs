fb65b6e6d7824bdb64c2f5eea34e31e10a099254
SCPC 1021 <SCPC1021@labs.upds>
2025-08-22 21:11:55 -0400

    agregando el DBcontext

--- Error404-Parkiando/Error404-Parkiando/Data/ParkingDBContext.cs
+++ Error404-Parkiando/Error404-Parkiando/Data/ParkingDBContext.cs
@@ -1,6 +1,22 @@
-﻿namespace Error404_Parkiando.Data
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Error404_Parkiando.Data
 {
-    public class ParkingDBContext
-{
-}
+    public class ParkingDbContext : DbContext
+    {
+        public ParkingDbContext(DbContextOptions<ParkingDbContext> options) : base(options) { }
+
+        public DbSet<Ticket> Tickets => Set<Ticket>();
+        public DbSet<ParkingSpot> ParkingSpots => Set<ParkingSpot>();
+
+        protected override void OnModelCreating(ModelBuilder m)
+        {
+            m.Entity<ParkingSpot>()
+             .HasIndex(x => x.Code).IsUnique();
+
+            m.Entity<Ticket>()
+             .Property(x => x.Amount)
+             .HasPrecision(18, 2);
+        }
+    }
 }
